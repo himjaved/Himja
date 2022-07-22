0f59f27af8468b1d7faa2613e1c5be521ce5dd69
HImja Ved <himja1414@gmail.com>
2022-07-22 13:28:55 +1000

    Added Heading

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
+        <h1 className="heading">LightningFlow Dashboard</h1>
         <Logo />
         <Map />
       </div>
